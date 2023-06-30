962cdf50a8b2262b9fc7a99c9bc3ac3c4f146915
savchuckvadim <savchuckvadim@gmail.com>
2023-06-30 18:42:52 +0300

    old stage

--- placement.php
+++ placement.php
@@ -23,4 +23,37 @@ require_once(__DIR__ . '/crest.php');
 
 
 ?>
-<!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet"><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="Web site created using create-react-app"/><link rel="apple-touch-icon" href="/logo192.png"/><link rel="manifest" href="/manifest.json"/><title>april-kp</title><script defer="defer" src="/static/js/main.06f180ab.js"></script><link href="/static/css/main.3fd9858a.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>
+
+<!doctype html><html lang="en" dir="ltr" itemscope itemtype="https://schema.org/WebPage" prefix="og:http://ogp.me/ns#"><head><meta charset="UTF-8"/><meta name="viewport" content="width=device-width,initial-scale=1"/><title>Webpack5 Max</title><link rel="canonical" href="https://example.com"/><link rel="icon" type="image/png" href="./icons/128.png"/><link rel="manifest" href="./manifest.json"/><meta name="application-name" content="Webpack5 Max"/><meta name="author" content="Igor Agapov"/><meta name="description" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta name="keywords" content="webpack, webpack5, boilerplate, template, max, config, bundler, bundle, javascript, react, reactjs, react.js, typescript, project, app"/><meta name="referrer" content="strict-origin"/><meta itemprop="name" content="Webpack5 Max"/><meta itemprop="description" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta itemprop="image" content="./icons/128.png"/><meta http-equiv="x-ua-compatible" content="ie=edge"/><meta name="msapplication-starturl" content="/"/><meta name="msapplication-tooltip" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta name="msapplication-TileColor" content="#f0f0f0"/><meta name="msapplication-config" content="browserconfig.xml"/><meta property="og:type" content="website"/><meta property="og:url" content="https://example.com"/><meta property="og:title" content="Webpack5 Max"/><meta property="og:description" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta property="og:image" content="./icons/512.png"/><meta property="og:locale" content="en_US"/><meta name="twitter:card" content="app"/><meta name="twitter:title" content="Webpack5 Max"/><meta name="twitter:description" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta name="twitter:url" content="https://example.com"/><meta name="twitter:image" content="./icons/512.png"/><meta name="apple-mobile-web-app-title" content="Webpack5 Max"/><meta name="apple-mobile-web-app-capable" content="yes"/><meta name="apple-mobile-web-app-status-bar-style" content="#333"/><link rel="apple-touch-icon" href="./icons/512.png"/><meta name="theme-color" content="#f0f0f0"/><meta name="color-scheme" content="light"/><meta name="mobile-web-app-capable" content="yes"/><script async src="https://www.googletagmanager.com/gtag/js?id=Google Analytics ID"></script><script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script><script src="//api.bitrix24.com/api/v1/"></script><script>let session = `<?php echo json_encode($_SESSION['PLACEMENT_OPTIONS']); ?>`
+    
+     </script><script>window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+
+      gtag('config', 'Google Analytics ID')</script><script type="application/ld+json">{
+        "@context": "http://schema.org",
+        "@type": "Webpage",
+        "url": "https://example.com",
+        "name": "April Kp",
+        "headline": "April Kp"",
+        "description": "make kp",
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": "https://example.com"
+        },
+        "publisher": {
+          "@type": "Person",
+          "name": "Igor Agapov"
+        },
+        "image": {
+          "@type": "ImageObject",
+          "url": "./icons/logo.svg",
+          "width": 1024,
+          "height": 512
+        },
+        "datePublished": "2021-02-27",
+        "keywords": "webpack, webpack5, boilerplate, template, max, config, bundler, bundle, javascript, react, reactjs, react.js, typescript, project, app"
+      }</script><script defer="defer" src="./js/index.a898abac9c1e2a216882.bundle.js"></script><script defer="defer" src="./js/runtime.3542db81979f6376b950.bundle.js"></script><script defer="defer" src="./js/react.7191512ba2df4ff3facb.bundle.js"></script><script defer="defer" src="./js/helpers.93e714bfb71c2a941d46.bundle.js"></script><link href="./css/index.5715d7c2661a552d10f6.css" rel="stylesheet"></head><body><noscript>Enable JavaScript, please</noscript><div id="root"></div></body></html>
+
