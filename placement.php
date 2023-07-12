a9450a4e099b1c931e69e5cb409d67df775955ff
savchuckvadim <savchuckvadim@gmail.com>
2023-07-12 17:18:09 +0300

    si

--- placement.php
+++ placement.php
@@ -23,7 +23,6 @@ require_once(__DIR__ . '/crest.php');
 
 
 ?>
-
 <!doctype html><html lang="en" dir="ltr" itemscope itemtype="https://schema.org/WebPage" prefix="og:http://ogp.me/ns#"><head><meta charset="UTF-8"/><meta name="viewport" content="width=device-width,initial-scale=1"/><title>Webpack5 Max</title><link rel="canonical" href="https://example.com"/><link rel="icon" type="image/png" href="./icons/128.png"/><link rel="manifest" href="./manifest.json"/><meta name="application-name" content="Webpack5 Max"/><meta name="author" content="Igor Agapov"/><meta name="description" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta name="keywords" content="webpack, webpack5, boilerplate, template, max, config, bundler, bundle, javascript, react, reactjs, react.js, typescript, project, app"/><meta name="referrer" content="strict-origin"/><meta itemprop="name" content="Webpack5 Max"/><meta itemprop="description" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta itemprop="image" content="./icons/128.png"/><meta http-equiv="x-ua-compatible" content="ie=edge"/><meta name="msapplication-starturl" content="/"/><meta name="msapplication-tooltip" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta name="msapplication-TileColor" content="#f0f0f0"/><meta name="msapplication-config" content="browserconfig.xml"/><meta property="og:type" content="website"/><meta property="og:url" content="https://example.com"/><meta property="og:title" content="Webpack5 Max"/><meta property="og:description" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta property="og:image" content="./icons/512.png"/><meta property="og:locale" content="en_US"/><meta name="twitter:card" content="app"/><meta name="twitter:title" content="Webpack5 Max"/><meta name="twitter:description" content="Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects"/><meta name="twitter:url" content="https://example.com"/><meta name="twitter:image" content="./icons/512.png"/><meta name="apple-mobile-web-app-title" content="Webpack5 Max"/><meta name="apple-mobile-web-app-capable" content="yes"/><meta name="apple-mobile-web-app-status-bar-style" content="#333"/><link rel="apple-touch-icon" href="./icons/512.png"/><meta name="theme-color" content="#f0f0f0"/><meta name="color-scheme" content="light"/><meta name="mobile-web-app-capable" content="yes"/><script async src="https://www.googletagmanager.com/gtag/js?id=Google Analytics ID"></script><script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script><script src="//api.bitrix24.com/api/v1/"></script><script>window.dataLayer = window.dataLayer || []
       function gtag() {
         dataLayer.push(arguments)
@@ -34,9 +33,9 @@ require_once(__DIR__ . '/crest.php');
         "@context": "http://schema.org",
         "@type": "Webpage",
         "url": "https://example.com",
-        "name": "April",
-        "headline": "April",
-        "description": "April",
+        "name": "Webpack5 Max",
+        "headline": "Webpack5 Max",
+        "description": "Full Webpack 5 Boilerplate for JavaScript, React & TypeScript projects",
         "mainEntityOfPage": {
           "@type": "WebPage",
           "@id": "https://example.com"
@@ -53,4 +52,4 @@ require_once(__DIR__ . '/crest.php');
         },
         "datePublished": "2021-02-27",
         "keywords": "webpack, webpack5, boilerplate, template, max, config, bundler, bundle, javascript, react, reactjs, react.js, typescript, project, app"
-      }</script><script defer="defer" src="./js/index.f05495f8b13ad1dfecc5.bundle.js"></script><script defer="defer" src="./js/runtime.da9023a0792009494071.bundle.js"></script><script defer="defer" src="./js/react.2885c5d3520f752f4663.bundle.js"></script><script defer="defer" src="./js/helpers.69b2249d6e3e67432444.bundle.js"></script><link href="./css/index.5715d7c2661a552d10f6.css" rel="stylesheet"></head><body><noscript>Enable JavaScript, please</noscript><div id="root"></div></body></html>
+      }</script><script defer="defer" src="./js/index.b021537468c6bc8fae1e.bundle.js"></script><script defer="defer" src="./js/runtime.da9023a0792009494071.bundle.js"></script><script defer="defer" src="./js/react.2885c5d3520f752f4663.bundle.js"></script><script defer="defer" src="./js/helpers.69b2249d6e3e67432444.bundle.js"></script><link href="./css/index.5715d7c2661a552d10f6.css" rel="stylesheet"></head><body><noscript>Enable JavaScript, please</noscript><div id="root"></div></body></html>
