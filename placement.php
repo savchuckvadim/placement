23b5da900f06141863212b677c6d832554f49cd9
savchuckvadim <savchuckvadim@gmail.com>
2023-10-04 10:15:29 +0300

    commit from python

--- placement.php
+++ placement.php
@@ -2,25 +2,6 @@
 require_once(__DIR__ . '/crest.php');
 
 
-
-// function displayValue($value)
-// {
-//   if (is_array($value)) {
-//     $result = '';
-//     foreach ($value as $item) $result .= $item . ', ';
-//     return $result;
-//   } else return $value;
-// }
-
-
-
-// $placement_options = json_decode($_REQUEST['PLACEMENT_OPTIONS'], true);
-
-// print_r($placement_options);
-
-// session_start();
-// $_SESSION['PLACEMENT_OPTIONS'] = $_REQUEST['PLACEMENT_OPTIONS'];
-
 ?>
 
 
