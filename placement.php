10878400aa9399941a6a554d37c0bee93f29853f
savchuckvadim <savchuckvadim@gmail.com>
2023-10-18 18:01:49 +0200

    fxd plcmnt

--- placement.php
+++ placement.php
@@ -5,4 +5,5 @@ require_once(__DIR__ . '/crest.php');
 ?>
 
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>React App</title><script defer="defer" src="./js/index.34ad2b821d5f45fde0e5.bundle.js"></script><script defer="defer" src="./js/runtime.231e1b8f62394a2b5c2f.bundle.js"></script><script defer="defer" src="./js/react.f0cd4aa775bd19072c5f.bundle.js"></script><script defer="defer" src="./js/helpers.5c11417907ee90f27907.bundle.js"></script><link href="./css/index.61660422f36dc1ff17e3.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>React App</title><script defer="defer" src="./js/index.74fa481bce1c19b3712c.bundle.js"></script><script defer="defer" src="./js/runtime.231e1b8f62394a2b5c2f.bundle.js"></script><script defer="defer" src="./js/react.f0cd4aa775bd19072c5f.bundle.js"></script><script defer="defer" src="./js/helpers.5c11417907ee90f27907.bundle.js"></script><link href="./css/index.61660422f36dc1ff17e3.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
