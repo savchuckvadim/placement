e48c603a8a5584ba7007ff5551e482f8b9d0b5bb
savchuckvadim <savchuckvadim@gmail.com>
2023-10-26 10:31:28 +0200

    Python 2023-10-26 10:31:27

--- placement.php
+++ placement.php
@@ -1,7 +1,7 @@
 <?
 require_once(__DIR__ . '/crest.php');
 ?>
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April App</title><script defer="defer" src="./js/index.3736c99eb03a9d74b452.bundle.js"></script><script defer="defer" src="./js/runtime.231e1b8f62394a2b5c2f.bundle.js"></script><script defer="defer" src="./js/react.f0cd4aa775bd19072c5f.bundle.js"></script><script defer="defer" src="./js/helpers.5c11417907ee90f27907.bundle.js"></script><link href="./css/index.32ae563fa4376c19b150.css" rel="stylesheet"></head><body><div id="root"></div><script>BX24.ready(function () {
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April App</title><script defer="defer" src="./js/index.566f78a3876c417b86d6.bundle.js"></script><script defer="defer" src="./js/runtime.231e1b8f62394a2b5c2f.bundle.js"></script><script defer="defer" src="./js/react.f0cd4aa775bd19072c5f.bundle.js"></script><script defer="defer" src="./js/helpers.5c11417907ee90f27907.bundle.js"></script><link href="./css/index.32ae563fa4376c19b150.css" rel="stylesheet"></head><body><div id="root"></div><script>BX24.ready(function () {
             BX24.init(function () {
                 BX24.resizeWindow(document.body.clientWidth,
                     document.getElementById("root").clientHeight);
