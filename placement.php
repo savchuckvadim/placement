4606a9e69ace04054e867e696127519f62788588
savchuckvadim <savchuckvadim@gmail.com>
2024-06-12 21:13:25 +0700

    add layout

---
+++ placement.php
@@ -0,0 +1,4 @@
+<?php
+require_once(__DIR__ . '/crest.php');
+?>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.eb1aef293d1f3d10a50f.js"></script><link href="css/main.189de283.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
