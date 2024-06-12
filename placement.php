a5d48a5c008530478b20fe1cbca37a37420d82d6
savchuckvadim <savchuckvadim@gmail.com>
2024-06-12 15:06:46 +0700

    fxd init

--- placement.php
+++ placement.php
@@ -1,4 +1,4 @@
 <?php
 require_once(__DIR__ . '/crest.php');
 ?>
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.85e74f23d026b463dcd5.js"></script><link href="css/main.41bc7f9d.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.d11e79cf2c4f394f7481.js"></script><link href="css/main.41bc7f9d.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
