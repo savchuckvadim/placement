6603076ed54ff9ce897c6baba060cd80a7fe4f7a
savchuckvadim <savchuckvadim@gmail.com>
2024-06-12 22:14:25 +0700

    fxd app style

--- placement.php
+++ placement.php
@@ -1,4 +1,4 @@
 <?php
 require_once(__DIR__ . '/crest.php');
 ?>
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.7eaac870407025539fc4.js"></script><link href="css/main.44198bf3.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.7eaac870407025539fc4.js"></script><link href="css/main.736888f1.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
