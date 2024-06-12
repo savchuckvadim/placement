094883431fefea8c766d380590165c8f096866d6
savchuckvadim <savchuckvadim@gmail.com>
2024-06-12 22:09:39 +0700

    FXD LAYOUT APP

--- placement.php
+++ placement.php
@@ -1,4 +1,4 @@
 <?php
 require_once(__DIR__ . '/crest.php');
 ?>
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.8f9542a0f71f386d19b3.js"></script><link href="css/main.3eaec5e8.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.7eaac870407025539fc4.js"></script><link href="css/main.44198bf3.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
