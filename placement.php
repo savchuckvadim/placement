0838c94f6a042ce58566497723083affba3ec283
savchuckvadim <savchuckvadim@gmail.com>
2024-06-17 14:09:23 +0700

    fxd deadline plan

--- placement.php
+++ placement.php
@@ -1,4 +1,4 @@
 <?php
 require_once(__DIR__ . '/crest.php');
 ?>
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.7dfdb1a63bb5a6e4de92.js"></script><link href="css/main.11c3aaca.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css/normalize.css"><title>Document</title><script defer="defer" src="main.188d4633fc88b7348d9c.js"></script><link href="css/main.b25118c1.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
