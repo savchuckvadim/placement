0acc0ff7c5de709e621bfd3baedcebc06419bca7
savchuckvadim <savchuckvadim@gmail.com>
2024-06-17 22:44:47 +0700

    fxd codes

--- placement.php
+++ placement.php
@@ -1,4 +1,4 @@
 <?php
 require_once(__DIR__ . '/crest.php');
 ?>
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.34667ed556520671a2b6.js"></script><link href="css/main.8d69e83f.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.6213fbb9a1bf98f27c1a.js"></script><link href="css/main.8d69e83f.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
