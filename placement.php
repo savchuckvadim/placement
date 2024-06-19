8c7e4d4c240f0d2650c7418abf9c620552d68c9e
savchuckvadim <savchuckvadim@gmail.com>
2024-06-19 20:08:53 +0700

    fxd preloader

--- placement.php
+++ placement.php
@@ -1,4 +1,4 @@
 <?php
 require_once(__DIR__ . '/crest.php');
 ?>
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.eb6a8381e03ce3487da0.js"></script><link href="css/main.ecd624e4.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.34ce4060a410d06ef546.js"></script><link href="css/main.ecd624e4.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
