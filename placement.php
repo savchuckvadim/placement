30617ab651ba0252d60d8a0f1a075951695736d5
savchuckvadim <savchuckvadim@gmail.com>
2024-06-19 22:24:49 +0700

    fxd place

--- placement.php
+++ placement.php
@@ -1,36 +1,36 @@
+
 <?php
-require_once(__DIR__ . '/crest.php');
+
 $placementOptions = [];
 if (isset($_POST['PLACEMENT']) && isset($_POST['PLACEMENT_OPTIONS'])) {
-    $placementOptionsJson = [
-    'options' => $_POST['PLACEMENT_OPTIONS'],
-    'placement' =>  $_POST[PLACEMENT]
-    ];
-    $placementOptions = json_decode($placementOptionsJson, true);
+    $options = $_POST['PLACEMENT_OPTIONS'];
+    $placement =  $_POST['PLACEMENT'];
 
-    // Проверяем, успешно ли прошло декодирование JSON
-    // if (json_last_error() !== JSON_ERROR_NONE) {
-    //     error_log("Ошибка декодирования JSON: " . json_last_error_msg());
-    // }
+    $dataToSend = json_encode([
+        'options' => json_decode($options, true), // Декодировать в массив для удобства работы, если необходимо
+        'placement' => json_decode($placement, true)
+    ]);
 }
 $url = "https://april-hook.ru/api/activity/test"; // URL API, куда вы отправляете запрос
 
 // Инициализация cURL сессии
 $ch = curl_init($url);
 
+// Настройка опций cURL для отправки JSON
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($placementOptions));
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($dataToSend));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
-    'Content-Length: ' . strlen(json_encode($placementOptions))
+    'Content-Length: ' . strlen(json_encode($dataToSend))
 ));
 
-
+// Выполнение запроса и получение ответа
 $response = curl_exec($ch);
 
 curl_close($ch);
 
+require_once(__DIR__ . '/crest.php');
+?>;
 
-?>
 <!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.d2104c5304dad5eea143.js"></script><link href="css/main.ecd624e4.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
