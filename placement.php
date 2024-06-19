768c0e949873036e5545c648bf7dd23712304a00
savchuckvadim <savchuckvadim@gmail.com>
2024-06-19 23:42:21 +0700

    tst pplan from type

--- placement.php
+++ placement.php
@@ -1,36 +1,7 @@
 
 <?php
 
-$placementOptions = [];
-if (isset($_POST['PLACEMENT']) && isset($_POST['PLACEMENT_OPTIONS'])) {
-    $options = $_POST['PLACEMENT_OPTIONS'];
-    $placement =  $_POST['PLACEMENT'];
-
-    $dataToSend = json_encode([
-        'options' => json_decode($options, true), // Декодировать в массив для удобства работы, если необходимо
-        'placement' => json_decode($placement, true)
-    ]);
-}
-$url = "https://april-hook.ru/api/activity/test"; // URL API, куда вы отправляете запрос
-
-// Инициализация cURL сессии
-$ch = curl_init($url);
-
-// Настройка опций cURL для отправки JSON
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($dataToSend));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen(json_encode($dataToSend))
-));
-
-// Выполнение запроса и получение ответа
-$response = curl_exec($ch);
-
-curl_close($ch);
-
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.d881dff40bf5b18ba254.js"></script><link href="css/main.ecd624e4.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.74330b144128ae6de82e.js"></script><link href="css/main.ecd624e4.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
