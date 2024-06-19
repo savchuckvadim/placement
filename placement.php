32b6e2cb9ee8bbfe23ed35e13fe8d2ef00c85c8f
savchuckvadim <savchuckvadim@gmail.com>
2024-06-19 14:41:04 +0700

    add preloader

--- placement.php
+++ placement.php
@@ -1,4 +1,4 @@
 <?php
 require_once(__DIR__ . '/crest.php');
 ?>
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.2100fb5ff16e8f4a655f.js"></script><link href="css/main.8d69e83f.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.66fa6f3594b5a7719f4b.js"></script><link href="css/main.8d69e83f.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
