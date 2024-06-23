0f3c4c5d8f38e55177eda916123d77f2c14b454c
savchuckvadim <savchuckvadim@gmail.com>
2024-06-23 12:26:15 +0300

    add deal placement

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.7d2316306c2c02b0559c.js"></script><link href="css/main.1a729621.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.2ee3d073bb3ddb99a239.js"></script><link href="css/main.1a729621.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
