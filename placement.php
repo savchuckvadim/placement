4e35dd1922d45e7bebc54d85b73daab8293c8d36
savchuckvadim <savchuckvadim@gmail.com>
2024-06-26 17:19:35 +0300

    tst pres deals

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.6f1b82dff3c40c225bdc.js"></script><link href="css/main.18d2eb3b.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.1f73a816e86de1806c1b.js"></script><link href="css/main.18d2eb3b.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
