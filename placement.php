2fb1fc144788ed7e078178a1dbf31d3a327f8f06
savchuckvadim <savchuckvadim@gmail.com>
2024-06-26 11:27:36 +0300

    fxd sleep

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.0c3cca133a3ee8c8ed32.js"></script><link href="css/main.18d2eb3b.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.7a6cbf4ce7a1ce812e08.js"></script><link href="css/main.18d2eb3b.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
