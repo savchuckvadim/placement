ecd3c0e8717e857a889c0be8ebc4edfaee556064
savchuckvadim <savchuckvadim@gmail.com>
2024-07-01 13:51:39 +0300

    testing departament

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.e6cae60b38dd4c8ca3e6.js"></script><link href="css/main.5bc5d968.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.4e121b8bcc84e7ec39ba.js"></script><link href="css/main.5bc5d968.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
