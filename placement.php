b90bb42da1b2d445d6336e471ca5c3a7da1fd109
savchuckvadim <savchuckvadim@gmail.com>
2024-07-02 12:07:40 +0300

    new layout test

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.4e121b8bcc84e7ec39ba.js"></script><link href="css/main.5bc5d968.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.6085fbf23fb6831e529f.js"></script><link href="css/main.fdfcbedf.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
