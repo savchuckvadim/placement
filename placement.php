776d5314a8369bb4449ba5e2c30eee83efbf5ba1
savchuckvadim <savchuckvadim@gmail.com>
2024-07-08 10:53:28 +0300

    add question

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.a72aca7a45e716fafec0.js"></script><link href="css/main.5ef92549.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.ef3d3ee6f6fd8bf82075.js"></script><link href="css/main.8f78bd5d.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
