72bf5095ae8679ba626554dde567510d56bb6371
savchuckvadim <savchuckvadim@gmail.com>
2024-07-17 14:38:20 +0300

    add timeline

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.08c7acdcced6bc0a513c.js"></script><link href="css/main.a632fde6.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.89a10c4c4cc6094fe142.js"></script><link href="css/main.a632fde6.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
