72ebfc2ca8816df68d5821a4c342fb0341a030c9
savchuckvadim <savchuckvadim@gmail.com>
2024-08-01 15:56:22 +0300

    fxd required fields

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.50c6a3485ec311a5ed03.js"></script><link href="css/main.4b10fc7e.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.9312f79ccb81ae25e7a4.js"></script><link href="css/main.4b10fc7e.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
