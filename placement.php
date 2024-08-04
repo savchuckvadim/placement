5361044eac85a2aa6da715aa5dee29673cd2d6a9
savchuckvadim <savchuckvadim@gmail.com>
2024-08-04 15:39:34 +0300

    fxd api

--- placement.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-
-require_once(__DIR__ . '/crest.php');
-?>;
-
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.be2b97fb11e22c44dcfa.js"></script><link href="css/main.b308b8a6.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
