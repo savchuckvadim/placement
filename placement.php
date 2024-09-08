2b9d7cbc1f9ce4aa2637bb901228d2af2765bd52
savchuckvadim <savchuckvadim@gmail.com>
2024-09-08 18:52:27 +0300

    fzd finish

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.e335ea793a2d861f8d65.js"></script><link href="css/main.758cb174.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.2ebc87d9a37f7d7c73f5.js"></script><link href="css/main.758cb174.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
