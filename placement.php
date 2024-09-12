0de6196d4cb75a074ae29655740fb16dbb6d92d7
savchuckvadim <savchuckvadim@gmail.com>
2024-09-12 17:18:58 +0300

    fxd call card layout

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.49ed4fd93f944445c7a2.js"></script><link href="css/main.83dcc5f8.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.5b1690e02c96d3efd3f4.js"></script><link href="css/main.f209889f.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
