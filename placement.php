ffad5ecd418c5c3720c5788e1998e61c4c3443d5
savchuckvadim <savchuckvadim@gmail.com>
2024-10-06 13:15:43 +0200

    with hook

--- placement.php
+++ placement.php
@@ -4,4 +4,4 @@
 require_once(__DIR__ . '/crest.php');
 ?>;
 
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>april-event</title><script defer="defer" src="main.2e70c7ddde662f936e5a.js"></script><link href="css/main.ba4a0a5c.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>april-event</title><script defer="defer" src="main.c0d7996946705ebf6b10.js"></script><link href="css/main.ba4a0a5c.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
